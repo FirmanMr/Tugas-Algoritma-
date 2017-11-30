db23bb3f1bd7a21c4e6298f9b09e76fa7fd4366e
Firman Moh Rizal <32824915+FirmanMr@users.noreply.github.com>
2017-11-30 08:56:19 -0500

    Add files via upload

---
+++ Masak Nasi.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main () {
+	printf(" Namasaya beras \n");
+	printf(" ambil baskom \n");
+	printf(" cuci beras \n");
+	printf(" Siapkan Beras \n");
+	printf(" Siapkan Beras \n");
+	printf(" Siapkan Beras \n");
+	printf(" Siapkan Beras \n");
+	printf(" Siapkan Beras \n");
+	printf(" Siapkan Beras \n");
+	printf(" Siapkan Beras \n");
+	printf(" Siapkan Beras \n");
+	printf(" Siapkan Beras \n");
+	printf(" Siapkan Beras \n");
+	printf(" Siapkan Beras \n");
+	printf(" Siapkan Beras \n");
+	printf(" Siapkan Beras \n");
+	return 0;
+}
